fd2b4864dd9ed9186fa1e6106ca9ce141f9ac616
sanmisanti <sanmisanti@gmail.com>
2023-10-25 21:14:00 -0300

    reserva Funcional

--- src/Components/Item/Item.jsx
+++ src/Components/Item/Item.jsx
@@ -1,19 +1,23 @@
-import { Link } from "react-router-dom"
-const Item = ({id,imagen,nombre,descripcion}) => {
+import { Link } from "react-router-dom";
+const Item = ({ id, imagen, nombre, descripcion }) => {
   return (
     <div key={id} className="card h-100">
-            <img src={imagen?imagen:'/defaultImage.jpg'} className="card-img-top img-thumbnail h-75" alt="imagen-card" />
-            <div className="card-body">
-                <h6>{nombre}</h6>
-                <p>{descripcion}</p>
-            </div>
-            <div className="card-footer text-center">
-                <Link to={`/detail/${id}`}>
-                    <button className="btn btn-outline-dark ">Detalle</button>
-                </Link>
-            </div>
-        </div>
-  )
-}
+      <img
+        src={imagen ? imagen : "/defaultImage.jpg"}
+        className="card-img-top img-thumbnail h-75"
+        alt="imagen-card"
+      />
+      <div className="card-body">
+        <h6>{nombre}</h6>
+        <p>{descripcion}</p>
+      </div>
+      <div className="card-footer text-center">
+        <Link to={`/detalleItem/${id}`}>
+          <button className="btn btn-outline-dark ">Detalle</button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
-export default Item
+export default Item;
--- src/Components/ItemDetail/ItemDetail.jsx
+++ src/Components/ItemDetail/ItemDetail.jsx
@@ -1,4 +1,8 @@
 import React, { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
+import Form from "react-bootstrap/Form";
 import {
   MDBCard,
   MDBCardBody,
@@ -7,13 +11,17 @@ import {
   MDBCardImage,
   MDBBtn,
 } from "mdb-react-ui-kit";
+import "./ItemDetail.css";
 import {
   collection,
   getDocs,
   getFirestore,
   limit,
+  setDoc,
+  addDoc,
   query,
   where,
+  doc,
 } from "firebase/firestore";
 import { app } from "../../firebase/config";
 
@@ -23,36 +31,134 @@ const ItemDetail = () => {
   const { id } = useParams();
   const [producto, setProducto] = useState({});
   const [isLoading, setIsLoading] = useState(true);
+  const [dateSince, setDateSince] = useState(new Date());
+  const [dateTo, setDateTo] = useState(new Date());
 
+  console.log("Fecha: ", new Date());
+
+  const dbFirestore = getFirestore(app);
   useEffect(() => {
-    const dbFirestore = getFirestore(app);
     const queryCollection = query(
       collection(dbFirestore, "propiedades"),
       where("id", "==", id)
     );
     getDocs(queryCollection)
-      .then((res) => setProducto(res.docs[0].data()))
+      .then((res) => {
+        setProducto(res.docs[0].data());
+        console.log(res.docs[0].data());
+      })
       .catch((error) => console.log(error))
       .finally(() => setIsLoading(false));
   }, []);
 
+  const handleReservar = () => {
+    addDoc(collection(dbFirestore, "reservas"), {
+      idPropiedad: `/propiedades/${id}`,
+      activo: true,
+      fechaDesde: dateSince,
+      fechaHasta: dateTo,
+    })
+      .then((res) => {
+        console.log("REserva creada con el Id: ", res.id);
+      })
+      .catch((error) => console.log("ERROR: ", error))
+      .finally(alert("FIJATE"));
+  };
+
   return (
-    <div>
+    <div className="background-card">
       {isLoading ? (
         <p>Cargando...</p>
       ) : (
         <div className="w-100 h-100 justify-content-center align-items-center d-flex flex-column">
-          <div className="border border-dark rounded w-50 h-50">
+          <div className="border border-dark rounded div-imagen p-0 m-0">
             <MDBCardImage
               src={producto.imagen || "/defaultImage.jpg"}
               position="top"
+              className="h-100 w-100"
               alt="..."
             />
           </div>
-          <MDBCard className="w-100">
+          <MDBCard className="card-detail">
             <MDBCardBody>
               <MDBCardTitle>{producto.nombre}</MDBCardTitle>
               <MDBCardText>{producto.descripcion}</MDBCardText>
+              <MDBCardTitle>Ubicación</MDBCardTitle>
+              <MDBCardText>País: {producto.pais}</MDBCardText>
+              <MDBCardText>Región: {producto.region}</MDBCardText>
+              <MDBCardText>
+                Dirección: {producto.calle} {producto.altura}
+              </MDBCardText>
+              <MDBCardTitle>Datos</MDBCardTitle>
+              <MDBCardText>
+                Cantidad de Cuartos: {producto.cantCuarto}
+              </MDBCardText>
+              <MDBCardText>
+                Cantidad de Baños:{" "}
+                {producto.cantBaños == "" ? 0 : producto.cantBaños}
+              </MDBCardText>
+              <MDBCardText>Area: {producto.area} m²</MDBCardText>
+              <MDBCardTitle>Datos Extra</MDBCardTitle>
+              <MDBCardText>
+                Aire Acondicionado: {producto.aire ? "Si" : "No"}{" "}
+              </MDBCardText>
+              <MDBCardText>
+                Estacionamiento: {producto.estacionamiento ? "Si" : "No"}{" "}
+              </MDBCardText>
+              <MDBCardText>
+                Lavarropa: {producto.lavarropa ? "Si" : "No"}{" "}
+              </MDBCardText>
+              <MDBCardText>WiFi: {producto.wifi ? "Si" : "No"} </MDBCardText>
+              {producto.tipo == "venta" ? (
+                <>
+                  <MDBCardTitle>Contactanos</MDBCardTitle>
+                  <input
+                    type="text"
+                    className="form-control mb-3"
+                    placeholder="Ingresá tu Mail"
+                  />
+                  <Form.Group
+                    className="mb-3"
+                    controlId="exampleForm.ControlTextarea1"
+                    placeholder="Ingresa tu consulta.."
+                  >
+                    <Form.Label>Escribí tu consulta...</Form.Label>
+                    <Form.Control as="textarea" className="mb-3 " />
+                    <Button variant="success" className="justify-content-end">
+                      Enviar
+                    </Button>
+                  </Form.Group>
+                </>
+              ) : (
+                <>
+                  <MDBCardTitle>Reservá</MDBCardTitle>
+                  <div className="d-flex mb-3">
+                    <div className="colCalendar d-flex flex-column align-items-center">
+                      <MDBCardTitle className="mb-3">Desde</MDBCardTitle>
+                      <Calendar
+                        onChange={setDateSince}
+                        value={dateSince}
+                        className="w-100"
+                      />
+                    </div>
+                    <div className="colCalendar  d-flex flex-column align-items-center">
+                      <MDBCardTitle className="mb-3">Hasta</MDBCardTitle>
+                      <Calendar
+                        onChange={setDateTo}
+                        value={dateTo}
+                        className="w-100"
+                      />
+                    </div>
+                  </div>
+                  <Button
+                    variant="success"
+                    className="justify-content-end"
+                    onClick={() => handleReservar()}
+                  >
+                    Reservar
+                  </Button>
+                </>
+              )}
             </MDBCardBody>
           </MDBCard>
         </div>
