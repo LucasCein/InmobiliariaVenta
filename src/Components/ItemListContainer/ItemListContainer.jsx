c35bc069bdfd69f452b5e9c86fdeaf66e00c4c96
sanmisanti <sanmisanti@gmail.com>
2023-11-09 21:27:02 -0300

    filtros

--- src/Components/ItemListContainer/ItemListContainer.jsx
+++ src/Components/ItemListContainer/ItemListContainer.jsx
@@ -1,29 +1,36 @@
-import { collection, getDocs, getFirestore, query, where } from 'firebase/firestore'
-import { useEffect, useState } from 'react'
-import CustomSpinner from '../CustomSpinner/CustomSpinner'
-import { useParams } from 'react-router-dom'
-import ItemList from '../ItemList/ItemList'
-import { app } from '../../firebase/config'
-import SideFilters from '../SideFilters/SideFilters'
+import {
+  collection,
+  getDocs,
+  getFirestore,
+  query,
+  where,
+} from "firebase/firestore";
+import { useEffect, useState } from "react";
+import CustomSpinner from "../CustomSpinner/CustomSpinner";
+import { useParams } from "react-router-dom";
+import ItemList from "../ItemList/ItemList";
+import { app } from "../../firebase/config";
+import SideFilters from "../SideFilters/SideFilters";
 const ItemListContainer = () => {
-  const [productos, setProductos] = useState([])
-  const [isLoading, setIsLoading] = useState(true)
-  const [countrySelected, setCountrySelected] = useState("")
-  const [regionSelected, setRegionSelected] = useState("")
-  const [estacionamiento, setEstacionamiento] = useState(false)
-  const [aireAcondicionado, setAireAcondicionado] = useState(false)
-  const [wifi, setWifi] = useState(false)
-  const [lavarropas, setLavarropas] = useState(false)
-  const [applyFilters, setApplyFilters] = useState(false)
-  const [isLoadingFilters,setIsLoadingFilters] = useState(false)
-  const { cpd } = useParams()
-  console.log('estacionamiento', estacionamiento)
-  console.log('aireAcondicionado', aireAcondicionado)
-  console.log('lavarropas', lavarropas)
-  console.log('wifi', wifi)
-  console.log('filter', applyFilters)
-  console.log('pais',countrySelected)
-  console.log('region',regionSelected)
+  const [productos, setProductos] = useState([]);
+  const [productosToShow, setProductosToShow] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [countrySelected, setCountrySelected] = useState("");
+  const [regionSelected, setRegionSelected] = useState("");
+  const [estacionamiento, setEstacionamiento] = useState(false);
+  const [aireAcondicionado, setAireAcondicionado] = useState(false);
+  const [wifi, setWifi] = useState(false);
+  const [lavarropas, setLavarropas] = useState(false);
+  const [applyFilters, setApplyFilters] = useState(false);
+  const [isLoadingFilters, setIsLoadingFilters] = useState(false);
+  const { cpd } = useParams();
+  console.log("estacionamiento", estacionamiento);
+  console.log("aireAcondicionado", aireAcondicionado);
+  console.log("lavarropas", lavarropas);
+  console.log("wifi", wifi);
+  console.log("filter", applyFilters);
+  console.log("pais", countrySelected);
+  console.log("region", regionSelected);
   // useEffect(() => {
   //   const dbFirestore = getFirestore(app)
   //   const queryCollection = query(collection(dbFirestore, 'propiedades'), where('estado', '==', 'disponible'), where('visible', '==', true))
@@ -37,31 +44,33 @@ const ItemListContainer = () => {
   //     .catch(error => console.log(error))
   //     .finally(() => setIsLoading(false))
 
-
   // }, [cpd, estacionamiento, aireAcondicionado, lavarropas, wifi, applyFilters])
   useEffect(() => {
-    
     const dbFirestore = getFirestore(app);
-    const baseQuery = query(collection(dbFirestore, 'propiedades'), where('estado', '==', 'disponible'), where('visible', '==', true));
+    const baseQuery = query(
+      collection(dbFirestore, "propiedades"),
+      where("estado", "==", "disponible"),
+      where("visible", "==", true)
+    );
 
     let queries = [baseQuery];
 
     if (applyFilters) {
       queries = [
-        query(baseQuery, where('estacionamiento', '==', estacionamiento)),
-        query(baseQuery, where('aireAcondicionado', '==', aireAcondicionado)),
-        query(baseQuery, where('lavarropas', '==', lavarropas)),
-        query(baseQuery, where('wifi', '==', wifi)),
-        
+        query(baseQuery, where("estacionamiento", "==", estacionamiento)),
+        query(baseQuery, where("aireAcondicionado", "==", aireAcondicionado)),
+        query(baseQuery, where("lavarropas", "==", lavarropas)),
+        query(baseQuery, where("wifi", "==", wifi)),
       ];
     }
 
     if (cpd) {
-      queries = queries.map(q => query(q, where('tipo', '==', cpd)));
-      
+      queries = queries.map((q) => query(q, where("tipo", "==", cpd)));
     }
-    if(regionSelected!==""){
-      queries = queries.map(q => query(q, where('region', '==', regionSelected)));
+    if (regionSelected !== "") {
+      queries = queries.map((q) =>
+        query(q, where("region", "==", regionSelected))
+      );
     }
     // Map to store unique documents
     const uniqueDocs = {};
@@ -76,29 +85,104 @@ const ItemListContainer = () => {
 
       const results = Object.values(uniqueDocs);
       setProductos(results);
+      setProductosToShow(results);
       console.log(results);
     };
 
-    fetchDocs().catch(error => console.log(error))
-      .finally(() => {setIsLoading(false),setIsLoadingFilters(false)});
+    fetchDocs()
+      .catch((error) => console.log(error))
+      .finally(() => {
+        setIsLoading(false), setIsLoadingFilters(false);
+        filtrar();
+      });
+  }, [
+    cpd,
+    estacionamiento,
+    aireAcondicionado,
+    lavarropas,
+    wifi,
+    applyFilters,
+    countrySelected,
+    regionSelected,
+  ]);
 
-  }, [cpd, estacionamiento, aireAcondicionado, lavarropas, wifi, applyFilters]);
+  const filtrar = () => {
+    console.log("productosToShow", productosToShow);
+    console.log("productos", productos);
+    if (aireAcondicionado) {
+      const productosFiltered = productosToShow.filter((p) => p.aire == true);
+      setProductosToShow(productosFiltered);
+      console.log("productosFiltered", productosFiltered);
+    }
+    if (lavarropas) {
+      const productosFiltered = productosToShow.filter(
+        (p) => p.lavarropa == true
+      );
+      setProductosToShow(productosFiltered);
+    }
+    if (estacionamiento) {
+      const productosFiltered = productosToShow.filter(
+        (p) => p.estacionamiento == true
+      );
+      setProductosToShow(productosFiltered);
+    }
+    if (wifi) {
+      const productosFiltered = productosToShow.filter((p) => p.wifi == true);
+      setProductosToShow(productosFiltered);
+    }
+    if (countrySelected != "") {
+      const productosFiltered = productosToShow.filter(
+        (p) => p.pais == countrySelected
+      );
+      setProductosToShow(productosFiltered);
+    }
+    console.log("regionr", regionSelected);
+    if (regionSelected != "") {
+      const productosFiltered = productosToShow.filter(
+        (p) => p.region == regionSelected
+      );
+      setProductosToShow(productosFiltered);
+    }
+  };
 
   return (
-    <section >
-      {isLoading ?
+    <section>
+      {isLoading ? (
         <CustomSpinner />
-        : <div>
-          {<h2 className="ms-4 mb-5 mt-2 ms-5">Inmuebles para {cpd.charAt(0).toUpperCase() + cpd.slice(1)}</h2>}
-          <div className='d-flex flex-row  w-100'>
-            <SideFilters setIsLoadingFilters={setIsLoadingFilters} aireAcondicionado={aireAcondicionado} lavarropas={lavarropas} wifi={wifi} estacionamiento={estacionamiento} setCountrySelected={setCountrySelected} setRegionSelected={setRegionSelected} countrySelected={countrySelected} regionSelected={regionSelected} setAireAcondicionado={setAireAcondicionado} setLavarropas={setLavarropas} setWifi={setWifi} setEstacionamiento={setEstacionamiento} setApplyFilters={setApplyFilters}></SideFilters>
-            {isLoadingFilters?<CustomSpinner />:<ItemList productos={productos} home={false} />}
+      ) : (
+        <div>
+          {
+            <h2 className="ms-4 mb-5 mt-2 ms-5">
+              Inmuebles para {cpd.charAt(0).toUpperCase() + cpd.slice(1)}
+            </h2>
+          }
+          <div className="d-flex flex-row  w-100">
+            <SideFilters
+              setIsLoadingFilters={setIsLoadingFilters}
+              aireAcondicionado={aireAcondicionado}
+              lavarropas={lavarropas}
+              wifi={wifi}
+              estacionamiento={estacionamiento}
+              setCountrySelected={setCountrySelected}
+              setRegionSelected={setRegionSelected}
+              countrySelected={countrySelected}
+              regionSelected={regionSelected}
+              setAireAcondicionado={setAireAcondicionado}
+              setLavarropas={setLavarropas}
+              setWifi={setWifi}
+              setEstacionamiento={setEstacionamiento}
+              setApplyFilters={setApplyFilters}
+            ></SideFilters>
+            {isLoadingFilters ? (
+              <CustomSpinner />
+            ) : (
+              <ItemList productos={productosToShow} home={false} />
+            )}
           </div>
         </div>
-      }
-
+      )}
     </section>
-  )
-}
+  );
+};
 
-export default ItemListContainer
+export default ItemListContainer;
